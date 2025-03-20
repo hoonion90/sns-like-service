7a78e89277f8b9f5a5a04400ed4185bd1d9321fb
fehoon.Hyeon <clearch21@gmail.com>
2025-03-20 21:53:36 +0900

    Initial commit: setup with spring-boot

---
+++ src/main/java/com/fehoon/sns/like/SnsLikeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.fehoon.sns.like;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnsLikeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnsLikeServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fehoon/sns/like/SnsLikeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fehoon.sns.like;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnsLikeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
